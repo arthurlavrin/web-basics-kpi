a566851faaea6eaf5c7cb36a105e494d59a18cf8
arthurlavrin <arthurlavrin@gmail.com>
2020-02-10 21:21:16 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -44,10 +44,8 @@ function adopt($text) {
 	return '=?UTF-8?B?'.Base64_encode($text).'?=';
 }
 
-$headers = "MIME-Version: 1.0" . PHP_EOL .
-"Content-Type: text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
-'Reply-To: '.$admin_email.'' . PHP_EOL;
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
 if ( mail($admin_email, adopt($form_subject), $message, $headers)) {
     echo("Email successfully sent to $admin_email...");
